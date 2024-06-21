b470c78087421296ef7f34627ca07a5b16a65992
UdayBhaskarSingipurapu <s.udaybhaskar2005@gmail.com>
2024-06-21 21:57:10 +0530

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./RootLayout";
+import Home from "./components/home/Home";
+import Register from "./components/register/Register";
+import Login from "./components/login/Login";
+import Aboutus from './components/aboutus/Aboutus'
+import UserProfile from './components/user-profile/UserProfile';
+import RoutingError from "./RoutingError";
+
+function App() {
+  const browserRouter = createBrowserRouter([
+    {
+      path: "",
+      element: <RootLayout />,
+      errorElement:<RoutingError />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+        },
+        {
+          path: "register",
+          element: <Register />,
+        },
+        {
+          path: "login",
+          element: <Login />,
+        },
+        {
+          path: "about",
+          element: <Aboutus />,
+        },
+        {
+          path:'user-profile',
+          element:<UserProfile />
+        }
+      ],
+    },
+  ]);
+
+  return (
+    <div className="main">
+      <RouterProvider router={browserRouter} />
+    </div>
+  );
+}
+
+export default App;
